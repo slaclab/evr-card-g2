6db3d203be9339068a69b261c545e01923043460
Larry Ruckman <ruckman@slac.stanford.edu>
2015-09-25 16:50:46 +0000

    -- 09/25/2015 (CED20008): Removed the dbRdAddr Synchronizer
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk/EvrCardG2/firmware@7975 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: de2272eb78542536d5aa99f5b4b4c40f733bedca

--- targets/EvrCardG2/Version.vhd
+++ targets/EvrCardG2/Version.vhd
@@ -3,9 +3,9 @@ use ieee.std_logic_1164.all;
 
 package Version is
 
-   constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"CED20007";  -- MAKE_VERSION
+   constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"CED20008";  -- MAKE_VERSION
 
-   constant BUILD_STAMP_C : string := "EvrCardG2: Vivado v2015.2 (x86_64) Built Thu Sep 24 15:57:46 PDT 2015 by ruckman";
+   constant BUILD_STAMP_C : string := "EvrCardG2: Vivado v2015.2 (x86_64) Built Fri Sep 25 09:40:57 PDT 2015 by ruckman";
 
 end Version;
 
@@ -21,6 +21,7 @@ end Version;
 -- 09/23/2015 (CED20006): Removed 4 shift registers in EvrV1EventReceiver for rxData & rxDataK
 --                        Registering the trigger outputs and set the SLEW = FAST
 -- 09/24/2015 (CED20007): Added 2 cycles address setup delay for BRAM's AXI-Lite read transactions
+-- 09/25/2015 (CED20008): Removed the dbRdAddr Synchronizer
 --
 -------------------------------------------------------------------------------
 
