8c6b67bc8001ea64ec455da68f764d13eb791b6d
Larry Ruckman <ruckman@slac.stanford.edu>
2015-10-19 20:28:49 +0000

    -- 10/19/2015 (CED20009): In EvrCardG2Trig.vhd: ODDR = "SAME_EDGE" and with clock MUX -- 10/19/2015 (CED2000A): In EvrCardG2Trig.vhd: ODDR = "SAME_EDGE" and bypass clock MUX
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk/EvrCardG2/firmware@8167 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 7f5ffa1439c4aa5274c5b3290de0dfae7c75e503

--- targets/EvrCardG2/Version.vhd
+++ targets/EvrCardG2/Version.vhd
@@ -3,9 +3,9 @@ use ieee.std_logic_1164.all;
 
 package Version is
 
-   constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"CED20008";  -- MAKE_VERSION
+   constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"CED2000A";  -- MAKE_VERSION
 
-   constant BUILD_STAMP_C : string := "EvrCardG2: Vivado v2015.2 (x86_64) Built Fri Sep 25 09:40:57 PDT 2015 by ruckman";
+   constant BUILD_STAMP_C : string := "EvrCardG2: Vivado v2015.3 (x86_64) Built Mon Oct 19 13:19:25 PDT 2015 by ruckman";
 
 end Version;
 
@@ -21,7 +21,10 @@ end Version;
 -- 09/23/2015 (CED20006): Removed 4 shift registers in EvrV1EventReceiver for rxData & rxDataK
 --                        Registering the trigger outputs and set the SLEW = FAST
 -- 09/24/2015 (CED20007): Added 2 cycles address setup delay for BRAM's AXI-Lite read transactions
--- 09/25/2015 (CED20008): Removed the dbRdAddr Synchronizer
+-- 09/25/2015 (CED20008): Removed the dbRdAddr Synchronizer 
+--                        Note: In EvrCardG2Trig.vhd: ODDR = "OPPOSITE_EDGE"
+-- 10/19/2015 (CED20009): In EvrCardG2Trig.vhd: ODDR = "SAME_EDGE" and with clock MUX
+-- 10/19/2015 (CED2000A): In EvrCardG2Trig.vhd: ODDR = "SAME_EDGE" and bypass clock MUX
 --
 -------------------------------------------------------------------------------
 
