394f8828678fb5530300cd83643c384c0c94c58d
Matt Weaver <weaver@slac.stanford.edu>
2016-01-24 20:07:34 +0000

    Connected V1/V2 Mux
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk/EvrCardG2/firmware@8882 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 40c40b0345612ceab00dcda9df7e541c23d01235

--- modules/common/general/rtl/EvrCardG2Trig.vhd
+++ modules/common/general/rtl/EvrCardG2Trig.vhd
@@ -5,7 +5,7 @@
 -- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-06-09
--- Last update: 2015-10-19
+-- Last update: 2016-01-22
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -52,17 +52,17 @@ begin
    -- Note: Legacy software requires inverting LCLS-I trigger
    --       and it's still TBD if we need to do the same for
    --       the LCLS-II trigger as well
-   -- trig <= not(trigIn(0)) when(evrModeSel = '0') else trigIn(1);
-   trig <= not(trigIn(0));
+   trig <= not(trigIn(0)) when(evrModeSel = '0') else trigIn(1);
+   -- trig <= not(trigIn(0));
 
-   -- BUFGMUX_inst : BUFGMUX
-   -- port map (
-   -- O  => clk,                     -- 1-bit output: Clock output
-   -- I0 => evrRecClk(0),            -- 1-bit input: Clock input (S=0)
-   -- I1 => evrRecClk(1),            -- 1-bit input: Clock input (S=1)
-   -- S  => evrModeSel);             -- 1-bit input: Clock select
+   BUFGMUX_inst : BUFGMUX
+   port map (
+     O  => clk,                     -- 1-bit output: Clock output
+     I0 => evrRecClk(0),            -- 1-bit input: Clock input (S=0)
+     I1 => evrRecClk(1),            -- 1-bit input: Clock input (S=1)
+     S  => evrModeSel);             -- 1-bit input: Clock select
 
-   clk <= evrRecClk(0);
+   -- clk <= evrRecClk(0);
 
    OR_TRIG :
    for i in 11 downto 0 generate
