2feba07edf39fc7231ecd9f8396e376b4da851cd
Larry Ruckman <ruckman@slac.stanford.edu>
2016-04-11 23:25:56 +0000

    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk/EvrCardG2/firmware@10000 0848cbd7-7569-0410-ab05-8cc0a0c67708
    Former-commit-id: 860004e26c672249e1ca9e75ef20468f3a9b15bf

---
+++ targets/LegacyEvrCardG2/vivado/debug_evr.tcl
@@ -0,0 +1,29 @@
+## Open the run
+open_run synth_1
+
+## Setup configurations
+set ilaName    u_ila_0
+
+## Create the core
+CreateDebugCore ${ilaName}
+
+## Set the record depth
+set_property C_DATA_DEPTH 32768 [get_debug_cores ${ilaName}]
+
+## Set the clock for the Core
+SetDebugCoreClk ${ilaName} {EvrCardG2Core_Inst/EvrCardG2LclsV1_Inst/EvrV1Core_Inst/GEN_BIG_ENDIAN.EvrV1Reg_Inst/axiClk}
+
+## Set the Probes
+ConfigProbe ${ilaName} {EvrCardG2Core_Inst/EvrCardG2LclsV1_Inst/EvrV1Core_Inst/GEN_BIG_ENDIAN.EvrV1Reg_Inst/irqActive}
+ConfigProbe ${ilaName} {EvrCardG2Core_Inst/EvrCardG2LclsV1_Inst/EvrV1Core_Inst/GEN_BIG_ENDIAN.EvrV1Reg_Inst/irqReq}
+ConfigProbe ${ilaName} {EvrCardG2Core_Inst/EvrCardG2LclsV1_Inst/EvrV1Core_Inst/GEN_BIG_ENDIAN.EvrV1Reg_Inst/status[intFlag][3]}
+ConfigProbe ${ilaName} {EvrCardG2Core_Inst/EvrCardG2LclsV1_Inst/EvrV1Core_Inst/GEN_BIG_ENDIAN.EvrV1Reg_Inst/status[intFlag][5]}
+ConfigProbe ${ilaName} {EvrCardG2Core_Inst/EvrCardG2LclsV1_Inst/EvrV1Core_Inst/GEN_BIG_ENDIAN.EvrV1Reg_Inst/r[config][latchTs]}
+ConfigProbe ${ilaName} {EvrCardG2Core_Inst/EvrCardG2LclsV1_Inst/EvrV1Core_Inst/GEN_BIG_ENDIAN.EvrV1Reg_Inst/r[rdEn]}
+ConfigProbe ${ilaName} {EvrCardG2Core_Inst/EvrCardG2LclsV1_Inst/EvrV1Core_Inst/GEN_BIG_ENDIAN.EvrV1Reg_Inst/r[wrEn]}
+
+## Delete the last unused port
+delete_debug_port [get_debug_ports [GetCurrentProbe ${ilaName}]]
+
+## Write the port map file
+write_debug_probes -force ${PROJ_DIR}/images/debug_probes.ltx
