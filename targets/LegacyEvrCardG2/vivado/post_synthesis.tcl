7078a230811038b0ea10a0d1e8114334a2b2234f
Larry Ruckman <ruckman@slac.stanford.edu>
2016-09-06 18:02:40 +0000

    Added SLAC licensing.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk/EvrCardG2/firmware@12268 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: d272f8753d98baf6739917ad687a2a882f7187e0

--- targets/LegacyEvrCardG2/vivado/post_synthesis.tcl
+++ targets/LegacyEvrCardG2/vivado/post_synthesis.tcl
@@ -1,3 +1,12 @@
+##############################################################################
+## This file is part of 'SLAC EVR Gen2'.
+## It is subject to the license terms in the LICENSE.txt file found in the 
+## top-level directory of this distribution and at: 
+##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+## No part of 'SLAC EVR Gen2', including this file, 
+## may be copied, modified, propagated, or distributed except according to 
+## the terms contained in the LICENSE.txt file.
+##############################################################################
 
 # User Post-Synthesis Script
 
