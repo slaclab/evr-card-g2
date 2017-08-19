f1fd6f3cd53be4b76bdc9c4280a045b9b893b03e
Matt Weaver <weaver@slac.stanford.edu>
2017-08-19 07:41:01 -0700

    Add checkout software

---
+++ software/app/tprtest.cc
@@ -0,0 +1,155 @@
+//
+//  Command-line driver of TPR configuration
+//
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <time.h>
+#include <stdio.h>
+
+#include "tpr.hh"
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace Tpr;
+
+static const double CLK_FREQ = 1300e6/7.;
+
+extern int optind;
+
+static void lcls2_link_test(TprReg&);
+
+static void usage(const char* p) {
+  printf("Usage: %s [options]\n",p);
+  printf("Options: -d <a..z> : /dev/tpr<arg>[0..a]\n");
+}
+
+int main(int argc, char** argv) {
+
+  extern char* optarg;
+  char tprid='a';
+
+  int c;
+  bool lUsage = false;
+  while ( (c=getopt( argc, argv, "d:h?")) != EOF ) {
+    switch(c) {
+    case 'd':
+      tprid  = optarg[0];
+      if (strlen(optarg) != 1) {
+        printf("%s: option `-r' parsing error\n", argv[0]);
+        lUsage = true;
+      }
+      break;
+    case 'h':
+      usage(argv[0]);
+      exit(0);
+    case '?':
+    default:
+      lUsage = true;
+      break;
+    }
+  }
+
+  if (optind < argc) {
+    printf("%s: invalid argument -- %s\n",argv[0], argv[optind]);
+    lUsage = true;
+  }
+
+  if (lUsage) {
+    usage(argv[0]);
+    exit(1);
+  }
+
+  {
+    char dev[16];
+    sprintf(dev,"/dev/tpr%c",tprid);
+    printf("Using tpr %s\n",dev);
+
+    int fd = open(dev, O_RDWR);
+    if (fd<0) {
+      perror("Could not open");
+      return -1;
+    }
+
+    void* ptr = mmap(0, sizeof(TprReg), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    if (ptr == MAP_FAILED) {
+      perror("Failed to map");
+      return -2;
+    }
+
+    TprReg& reg = *reinterpret_cast<TprReg*>(ptr);
+    reg.xbar.setEvr( XBar::StraightIn );
+    reg.xbar.setTpr( XBar::StraightIn );
+
+    //
+    //  Validate LCLS-II link
+    //
+    lcls2_link_test(reg);
+
+    //
+    //  Capture series of timing frames (show table)
+    //
+
+    //
+    //  Generate triggers (what device can digitize them)
+    //
+
+    //
+    //  Validate LCLS-I link
+
+    //
+    //  Capture series of timing frames (show table)
+    //
+
+    //
+    //  Generate triggers
+    //
+
+  }
+
+  return 0;
+}
+
+void lcls2_link_test(TprReg& reg)
+{
+    reg.tpr.clkSel(true);
+    reg.tpr.rxPolarity(false);
+    usleep(100000);
+    reg.tpr.resetCounts();
+    unsigned rxclks0 = reg.tpr.RxRecClks;
+    unsigned txclks0 = reg.tpr.TxRefClks;
+    usleep(1000000);
+    unsigned rxclks1 = reg.tpr.RxRecClks;
+    unsigned txclks1 = reg.tpr.TxRefClks;
+    unsigned sofCnts = reg.tpr.SOFcounts;
+    unsigned crcErrs = reg.tpr.CRCerrors;
+    unsigned decErrs = reg.tpr.RxDecErrs;
+    unsigned dspErrs = reg.tpr.RxDspErrs;
+    printf("RxRecClkFreq: %7f.2  %s\n", 
+           double(rxclks1-rxclks0)*1.e-6,
+           (rxclks1 > rxclks0+184000000 &&
+            rxclks1 < rxclks0+187000000) ? "PASS":"FAIL");
+    printf("TxRefClkFreq: %7f.2  %s\n", 
+           double(txclks1-txclks0)*1.e-6,
+           (rxclks1 > rxclks0+184000000 &&
+            rxclks1 < rxclks0+187000000) ? "PASS":"FAIL");
+    printf("SOFcounts   : %u  %s\n",
+           sofCnts,
+           (sofCnts > 920000 &&
+            sofCnts < 940000) ? "PASS":"FAIL");
+    printf("CRCerrors   : %u  %s\n",
+           crcErrs,
+           crcErrs == 0 ? "PASS":"FAIL");
+    printf("DECerrors   : %u  %s\n",
+           decErrs,
+           decErrs == 0 ? "PASS":"FAIL");
+    printf("DSPerrors   : %u  %s\n",
+           dspErrs,
+           dspErrs == 0 ? "PASS":"FAIL");
+}
