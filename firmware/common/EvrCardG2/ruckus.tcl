52789b9ad4ffa5038ce45dc87cba2114a12607a5
Matt Weaver <weaver@slac.stanford.edu>
2018-08-14 16:12:02 -0700

    Use xci for XVC

--- firmware/common/EvrCardG2/ruckus.tcl
+++ firmware/common/EvrCardG2/ruckus.tcl
@@ -3,6 +3,6 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -dir  "$::DIR_PATH/general/rtl"
-loadSource -dir  "$::DIR_PATH/general/coregen"
+loadIpCore -dir  "$::DIR_PATH/general/coregen"
 loadSource -dir  "$::DIR_PATH/pci/rtl"
 loadSource -dir  "$::DIR_PATH/pci/coregen"
