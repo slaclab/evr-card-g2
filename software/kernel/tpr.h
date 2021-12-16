2df2775cd20e152335a1e77dbc902d9248b583f5
Kukhee Kim <khkim@slac.stanford.edu>
2021-12-16 11:28:19 -0800

    add volatile for registers

--- software/kernel/tpr.h
+++ software/kernel/tpr.h
@@ -129,31 +129,31 @@ struct TprQueues {
 #define TPR_SH_MEM_WINDOW   ((sizeof(struct TprQueues) + PAGE_SIZE) & PAGE_MASK)
 
 struct TprReg {
-  __u32 reserved_0[0x10000>>2];
-  __u32 FpgaVersion;
-  __u32 reserved_04[(0x30000>>2)-1];
-  __u32 xbarOut[4]; // 0x40000
-  __u32 reserved_40010[(0x20000>>2)-4];
-  __u32 irqControl; // 0x60000
-  __u32 irqStatus;
-  __u32 reserved_60008[0x3f8>>2];
+  volatile  __u32 reserved_0[0x10000>>2];
+  volatile  __u32 FpgaVersion;
+  volatile  __u32 reserved_04[(0x30000>>2)-1];
+  volatile  __u32 xbarOut[4]; // 0x40000
+  volatile  __u32 reserved_40010[(0x20000>>2)-4];
+  volatile  __u32 irqControl; // 0x60000
+  volatile  __u32 irqStatus;
+  volatile  __u32 reserved_60008[0x3f8>>2];
   //  PcieRxDesc   0x60400
-  __u32 rxFree    [16];   // WO 0x400 Write Desc/Address
-  __u32 rxFreeStat[16];   // RO 0x440 Free FIFO (31:31 full, 30:30 valid, 9:0 count)
-  __u32 reserved  [32];
-  __u32 rxMaxFrame;       // RW 0x500 (31:31 freeEnable, 23:0 maxFrameSize)
-  __u32 rxFifoSize;       // RW 0x504 Buffers per FIFO
-  __u32 rxCount ;         // RO 0x508 (rxCount)
-  __u32 lastDesc;         // RO 0x50C (lastDesc)
+  volatile  __u32 rxFree    [16];   // WO 0x400 Write Desc/Address
+  volatile  __u32 rxFreeStat[16];   // RO 0x440 Free FIFO (31:31 full, 30:30 valid, 9:0 count)
+  volatile  __u32 reserved  [32];
+  volatile  __u32 rxMaxFrame;       // RW 0x500 (31:31 freeEnable, 23:0 maxFrameSize)
+  volatile  __u32 rxFifoSize;       // RW 0x504 Buffers per FIFO
+  volatile  __u32 rxCount ;         // RO 0x508 (rxCount)
+  volatile  __u32 lastDesc;         // RO 0x50C (lastDesc)
 
-  __u32 reserved_60510[((0x20000-0x510)>>2)];
+  volatile  __u32 reserved_60510[((0x20000-0x510)>>2)];
   struct ChReg {    // 0x80000
-    __u32 control;
-    __u32 reserved[0x3ff];
+    volatile  __u32 control;
+    volatile  __u32 reserved[0x3ff];
   } channel[32];
   struct TrReg {    // 0xa0000
-    __u32 control;
-    __u32 reserved[0x3ff];
+    volatile  __u32 control;
+    volatile  __u32 reserved[0x3ff];
   } trigger[32];
 };
 
