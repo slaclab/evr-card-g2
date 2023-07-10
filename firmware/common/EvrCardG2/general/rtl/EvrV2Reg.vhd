723b5e1310f2fe7cbac9a9259a4870ccf9022ead
Matt Weaver <weaver@slac.stanford.edu>
2023-07-10 11:46:53 -0700

    fix sensitivity list

--- firmware/common/EvrCardG2/general/rtl/EvrV2Reg.vhd
+++ firmware/common/EvrCardG2/general/rtl/EvrV2Reg.vhd
@@ -100,7 +100,7 @@ begin  -- mapping
     end if;
   end process;
 
-  process (r,axilReadMaster,axilWriteMaster,axiRst,gtxDebug,eventCount,irqReq,partitionAddr)
+  process (r,axilReadMaster,axilWriteMaster,axiRst,gtxDebug,eventCount,irqReq,partitionAddr,dmaDrops)
     variable v  : RegType;
     variable ep : AxiLiteEndPointType;
     variable sReg : slv(0 downto 0);
